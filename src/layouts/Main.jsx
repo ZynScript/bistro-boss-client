e03bc383cb218e94466619f76910d682c4c65e25
zihad <zynscriptofficial@gmail.com>
2023-06-12 22:42:27 +0600

    carousel added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Outlet} from "react-router-dom";
-import Footer from "./pages/Shared/Footer";
-import NavBar from "./pages/Shared/NavBar";
+import Footer from "../pages/Shared/Footer";
+import NavBar from "../pages/Shared/NavBar";
 
 const Main = () => {
   return (
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,6 @@
 import {createBrowserRouter} from "react-router-dom";
 import Main from "../layouts/Main";
-import Home from "../layouts/pages/Home/Home";
+import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
   {
